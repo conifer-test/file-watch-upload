448301d6c560cee70d77a3c321eb062d3b553015
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-08-02 14:30:19 -0700

    Implement test video upload to S3

--- observer.js
+++ observer.js
@@ -5,19 +5,18 @@ const observer = new Observer();
 const config = JSON.parse(fs.readFileSync('/app/.conifer/conifer-config.json'));
 
 // const folder = `${config.testDirectory}/results`;
-const folder = `/app/cypress/results`;
 
 const putInBucket = require('./s3-test-result-uploader');
 const updateExisitingTestFileInDynamo = require('./dynamoDB-test-result-uploader');
 
-observer.on('file-added', (log) => {
+observer.on('json-file-added', (log) => {
   // print error message to console
   console.log(`File was added: ${log.filePath}`);
 
   const uuid = path.parse(log.filePath).name;
 
   // Export file to s3 bucket
-  putInBucket(log.filePath, uuid, config.bucketName);
+  putInBucket(log.filePath, uuid, config.bucketName, '.json');
 
   // TODO: Need to test and ensure this file path matches file globbing which is the base used to create primary keys
   const fullFilePath = `${log.filePath}`;
@@ -28,4 +27,18 @@ observer.on('file-added', (log) => {
   }
 });
 
-observer.watchFolder(folder);
+observer.on('mp4-file-added', (log) => {
+  // print error message to console
+  console.log(`File was added: ${log.filePath}`);
+
+  const uuid = path.parse(log.filePath).name;
+
+  // Export file to s3 bucket
+  putInBucket(log.filePath, uuid, config.bucketName, '.mp4');
+});
+
+const resultsFolder = '/app/cypress/results';
+observer.watchFolder(resultsFolder);
+
+const videosFolder = '/app/cypress/videos';
+observer.watchFolder(videosFolder);
--- test-folder-watcher.js
+++ test-folder-watcher.js
@@ -11,7 +11,18 @@ class Observer extends EventEmitter {
       console.log(
         `[${new Date().toLocaleString()}] Watching for folder changes on: ${folder}`
       );
-      const watcher = chokidar.watch(folder, { persistent: true });
+
+      const watcher = chokidar.watch(
+        folder,
+        {
+          persistent: true,
+          awaitWriteFinish: {
+            stabilityThreshold: 2000,
+            pollInterval: 100
+          }
+        }
+      );
+
       watcher.on('add', async filePath => {
         // Only fire an event if a .json file is created
         if (filePath.includes('.json')) {
@@ -22,7 +33,18 @@ class Observer extends EventEmitter {
           // emit an event when new file has been added
           const DELAY_BEFORE_WRITE = 1000;
           setTimeout(() => {
-            this.emit('file-added', {
+            this.emit('json-file-added', {
+              filePath
+            });
+          }, DELAY_BEFORE_WRITE);
+        } else if (filePath.includes('.mp4') && !filePath.includes('-compressed.mp4') && !filePath.includes('.mp4.meta')) {
+          console.log(
+            `[${new Date().toLocaleString()}] ${filePath} has been added.`
+          );
+
+          const DELAY_BEFORE_WRITE = 5000;
+          setTimeout(() => {
+            this.emit('mp4-file-added', {
               filePath
             });
           }, DELAY_BEFORE_WRITE);
